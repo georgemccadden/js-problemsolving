5322855590754186cadc66d91f120a167c318b7e
George McCadden III <thethirdone.g@gmail.com>
2020-11-20 12:52:47 -0800

    Made some algorithm changes

--- index.js
+++ index.js
@@ -12,20 +12,21 @@
 // Brute Force Algorithm :
 
 // - Check to see if the lengths of the passed in strings are equal, if so continue. If not, return false.
-// - Create hashmaps with the letters of the first and second strings by iterating through it entirely and updating the letter's count at each instance.
-// - Check each letter of the second string to see if it has that same letter amount, if so, return true. If not, return false.
+// - Create a hashmap with the letters of the first string by iterating through it entirely and updating the letter's count at each instance.
+// - Check each letter of the second string to see if it that letter is present in the hashmap that, if so, return decrement it. If not, return false.
 
 
 // Solution :
 
 function areTheyAnagrams(string1, string2) {
-    let letterBank1 = {};
-    let letterBank2 = {};
+    let letterBank = {};
 
     if (string1.length !== string2.length) {
         return false;
     }
 
-    // 
+    // Create the hashmap for the first string
+
+
 
 }
