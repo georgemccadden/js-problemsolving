040c5da113cd920a125fbf030c531b30f36b2a54
George McCadden III <thethirdone.g@gmail.com>
2020-11-20 12:44:16 -0800

    Creating the hashmaps

--- index.js
+++ index.js
@@ -14,3 +14,18 @@
 // - Check to see if the lengths of the passed in strings are equal, if so continue. If not, return false.
 // - Create hashmaps with the letters of the first and second strings by iterating through it entirely and updating the letter's count at each instance.
 // - Check each letter of the second string to see if it has that same letter amount, if so, return true. If not, return false.
+
+
+// Solution :
+
+function areTheyAnagrams(string1, string2) {
+    let letterBank1 = {};
+    let letterBank2 = {};
+
+    if (string1.length !== string2.length) {
+        return false;
+    }
+
+    // 
+
+}
