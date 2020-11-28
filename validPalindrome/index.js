57f29975f589b4620eb44bace07d183dcb9a0a43
George McCadden III <thethirdone.g@gmail.com>
2020-11-28 15:21:23 -0800

    Giving a clear example

--- validPalindrome/index.js
+++ validPalindrome/index.js
@@ -1,3 +1,13 @@
 // Problem Domain :
 
 // Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+
+// Example :
+
+// Input -> "A man, a plan, a canal: Panama"
+// Output -> true
+
+// Input -> "race a car"
+// Output -> false
+
