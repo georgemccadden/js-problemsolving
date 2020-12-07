4159f6f71672e482e1afaf25d6c0b11d455a6ba3
George McCadden III <thethirdone.g@gmail.com>
2020-12-07 07:29:47 -0800

    Giving a clear example

--- repeatedString/index.js
+++ repeatedString/index.js
@@ -1,3 +1,10 @@
 // Problem Domain :
 
 // There is a string `a` of lowercase English letters that is repeated infinitely many times. Given an integer `n`, find and print the number of letter a's in the first n letters of the infinite string.
+
+// Example :
+
+// s = `abcac` -> `abcacabcacabcacabcacabcac`...
+// n = 10
+// Output -> 4
+// The substring you consider is `abcacabcac`, the first 10 characters of the infinite string. There are 4 occurrences of `a` in the substring.
