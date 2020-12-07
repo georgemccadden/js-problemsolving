6523c341c874343f051544aa6df78c700f8d3125
George McCadden III <thethirdone.g@gmail.com>
2020-12-07 07:45:31 -0800

    Wrote a clear algorithm

--- repeatedString/index.js
+++ repeatedString/index.js
@@ -8,3 +8,12 @@
 // n = 10
 // Output -> 4
 // The substring you consider is `abcacabcac`, the first 10 characters of the infinite string. There are 4 occurrences of `a` in the substring.
+
+// Algorithm :
+
+// - The function should accept a string and a number
+// - Copy and concatenate the passed in string until the length is at least `n` characters long
+// - Constrain the string to the first `n` characters
+// - Loop through the entire string and keep count of the letter `a`
+// - Return the count
+// - If there is no letter `a` return -1
