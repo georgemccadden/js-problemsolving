b4c7029555adbba7bdd9da7ee6435a80230ebf30
George McCadden III <thethirdone.g@gmail.com>
2020-12-07 07:21:05 -0800

    Stating the problem domain

---
+++ repeatedString/index.js
@@ -0,0 +1,3 @@
+// Problem Domain :
+
+// There is a string `a` of lowercase English letters that is repeated infinitely many times. Given an integer `n`, find and print the number of letter a's in the first n letters of the infinite string.
