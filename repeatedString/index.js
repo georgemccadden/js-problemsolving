503937be48dd4cfead2ff9c70622fad23c557c38
George McCadden III <thethirdone.g@gmail.com>
2020-12-07 14:06:29 -0800

    This was complex

--- repeatedString/index.js
+++ repeatedString/index.js
@@ -11,9 +11,25 @@
 
 // Algorithm :
 
-// - The function should accept a string and a number
-// - Copy and concatenate the passed in string until the length is at least `n` characters long
-// - Constrain the string to the first `n` characters
-// - Loop through the entire string and keep count of the letter `a`
-// - Return the count
-// - If there is no letter `a` return -1
+// - Create a variable for the substring
+// - Determine how many a's are in a valid substring
+// - Determine the remainder
+// - Look for any a's in the remainder
+// - If there is no remainder with the `n` and the string then return the a's of that substring, if there is, add the the remainder of a's
+
+// Code :
+
+function repeatedString(s, n) {
+	const subString = Math.floor(n / s.length);
+	const subStringOfAs = (s.match(/a/g) || []).length;
+	const remainder = n % s.length;
+	const remainderOfAs = s.split('').slice(0, remainder).filter((letter) => letter === `a`).length;
+
+	// If the length of the string is divisible by `n` with no remainder then there's no need to take care of remainders
+	return n % s.length === 0 ? n / s.length * subStringOfAs : subString * subStringOfAs + remainderOfAs;
+}
+
+// Big O :
+
+// Runtime Complexity -> O(n)
+// Space Complexity -> O(1)
