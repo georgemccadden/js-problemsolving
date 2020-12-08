85f4ee628b23e4e6755054fde8c20d9e1eb01c35
George McCadden III <thethirdone.g@gmail.com>
2020-12-08 13:18:42 -0800

    Changed some things
    I better understand the problem domain and now I am giving an example

--- minimumBribes/index.js
+++ minimumBribes/index.js
@@ -1,6 +1,8 @@
 // Problem Domain :
 
-// Determine the how many bribes took place amongst those in queue for a rollercoaster when given these parameters :
-// - riders in queue must always keep an identifier of where they started in the queue
-// - riders may bribe two others (at most) and it can only be those in front of them or behind them
-// - there are eight positions in this queue and as riders and their positions increment by one from one at the front of the line
+// Determine the minimum number of bribes that took place amongst those in queue for a rollercoaster to get the queue into to its original state. Here are the parameters ->
+// - Riders in queue must always keep an identifier of where they started in the queue
+// - Riders may bribe two others (at most) and it can only be those in front of them or behind them
+// - There are eight positions in this queue and as riders and their positions increment by one from one at the front of the line
+
+// Example :
