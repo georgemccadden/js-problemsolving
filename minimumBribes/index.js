6805c1a8952bf9fcbb3a677404dca51f10640b49
George McCadden III <thethirdone.g@gmail.com>
2020-12-11 11:27:00 -0800

    Preparing a brute force algorithm

--- minimumBribes/index.js
+++ minimumBribes/index.js
@@ -1,11 +1,11 @@
-// Problem Domain :
+// ** PROBLEM DOMAIN :
 
 // Determine the minimum number of bribes that took place amongst those in queue for a rollercoaster to get the queue back to its original state. Here are the parameters ->
 // - Riders in queue must always keep an identifier of where they started in the queue
 // - Riders may bribe two others (at most) and it can only be those in front of them or behind them
 // - There are eight positions in this queue and riders' positions increment by one from one at the front of the line
 
-// Example :
+// ** EXAMPLE 1 :
 
 // Input ->
 
@@ -17,7 +17,7 @@
 
 // 3
 
-// Example 2 :
+// ** EXAMPLE 2 :
 
 // Input ->
 
@@ -27,3 +27,5 @@
 // Output ->
 
 // Too chaotic
+
+// ** BRUTE FORCE ALROGITHM :
