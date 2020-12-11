86f3e48ea1ee5daaaa5e3086b1bdb70e7f5fe571
George McCadden III <thethirdone.g@gmail.com>
2020-12-11 11:14:09 -0800

    Clarifying language

--- minimumBribes/index.js
+++ minimumBribes/index.js
@@ -1,6 +1,6 @@
 // Problem Domain :
 
-// Determine the minimum number of bribes that took place amongst those in queue for a rollercoaster to get the queue into to its original state. Here are the parameters ->
+// Determine the minimum number of bribes that took place amongst those in queue for a rollercoaster to get the queue back to its original state. Here are the parameters ->
 // - Riders in queue must always keep an identifier of where they started in the queue
 // - Riders may bribe two others (at most) and it can only be those in front of them or behind them
 // - There are eight positions in this queue and as riders and their positions increment by one from one at the front of the line
