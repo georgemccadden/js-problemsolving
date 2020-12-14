45056f6c3ddbc6bb88ca2eb48db806b69a548363
George McCadden III <thethirdone.g@gmail.com>
2020-12-14 15:45:04 -0800

    Stating the problem domain

---
+++ climbingStairs/index.js
@@ -0,0 +1,5 @@
+// ** PROBLEM DOMAIN
+
+// Determine how many distinct ways you climb to the top of the stairs when given these parameters ->
+// - You can either climb one or two steps at a time
+// - It takes `n` steps to reach the top
