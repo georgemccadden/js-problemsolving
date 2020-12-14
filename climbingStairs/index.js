0e1a203c821e958bc4e557c7aff83c95a770943a
George McCadden III <thethirdone.g@gmail.com>
2020-12-14 15:49:41 -0800

    Giving clear examples

--- climbingStairs/index.js
+++ climbingStairs/index.js
@@ -3,3 +3,15 @@
 // Determine how many distinct ways you climb to the top of the stairs when given these parameters ->
 // - You can either climb one or two steps at a time
 // - It takes `n` steps to reach the top
+
+// ** EXAMPLE 1
+
+// Input -> n = 2
+// Output -> 2
+// Explanation -> There are two ways to climb to the top of the stairs (1. One step + one step / 2. Two steps)
+
+// ** EXAMPLE 2
+
+// Input -> n = 3
+// Output -> 3
+// Explanation -> There are three ways to climb to the top of the stairs (1. One step + one step + one step / 2. One step + two steps / 3. Two steps + one step)
