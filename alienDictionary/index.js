e30d6c784fc5869598751f65ed9668b66bb8a1f7
George McCadden III <thethirdone.g@gmail.com>
2020-12-15 18:58:08 -0800

    Stating the problem domain

---
+++ alienDictionary/index.js
@@ -0,0 +1,7 @@
+// ** Problem Domain :
+
+// In an alien language, surprisinglythey also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+// Determine if the words and the particular order in which they are given are sorted lexicographicaly in this alien language. These are the constraints ->
+// - This function will receive a sequence of words and the order of the alphabet
+// - You must return `true` if the words are sorted lexicographicaly
