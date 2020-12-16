051708aa40c7cc30424d080343afc9315f0fa201
George McCadden III <thethirdone.g@gmail.com>
2020-12-15 19:03:43 -0800

    Giving a clear example
    It is all about alphabetical order although it is determined by the odd alien language

--- alienDictionary/index.js
+++ alienDictionary/index.js
@@ -5,3 +5,9 @@
 // Determine if the words and the particular order in which they are given are sorted lexicographicaly in this alien language. These are the constraints ->
 // - This function will receive a sequence of words and the order of the alphabet
 // - You must return `true` if the words are sorted lexicographicaly
+
+// ** Example 1 :
+
+// Input -> words = [`hello`, `leetcode`], order = `hlabcdefgijkmnopqrstuvwxyz`
+// Output -> true
+// Explanation -> As `h` comes before `l` in this language, then the sequence is sorted.
