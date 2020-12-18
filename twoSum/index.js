8848f140f1a1f5c1035c8f7532814687856c39a4
George McCadden III <thethirdone.g@gmail.com>
2020-12-17 18:59:22 -0800

    Giving clear examples

--- twoSum/index.js
+++ twoSum/index.js
@@ -4,3 +4,15 @@
 // - You may assume that each input would have exactly ONE solution
 // - You may not use the same element twice
 // - There must be at least two elements
+
+// ** EXAMPLE 1 :
+
+// Input -> nums = [2, 7, 11, 15], target = 9
+// Output -> [0, 1]
+// Explanation -> Because nums[0] + nums[1] == 9, [0, 1] is returned
+
+// ** EXAMPLE 2 :
+
+// Input -> nums = [3, 2, 4], target = 6
+// Output -> [1, 2]
+// Explanation -> Because nums[1] + nums[2] == 6, [1, 2] is returned
