c6c146cada9e51dbb1e0229cccff6c62b15432fc
George McCadden III <thethirdone.g@gmail.com>
2020-12-17 21:59:18 -0800

    This solution passed on Leetcode

--- twoSum/index.js
+++ twoSum/index.js
@@ -16,3 +16,28 @@
 // Input -> nums = [3, 2, 4], target = 6
 // Output -> [1, 2]
 // Explanation -> Because nums[1] + nums[2] == 6, [1, 2] is returned
+
+// ** BRUTE FORCE APPROACH :
+
+// - First look at the passed in `target` integer
+// - Loop through the array and, for each element, loop until you find another value that equals to the target
+// Complexity Analysis -> Big O would be O(n^2) time because, for each element, you must loop through the rest of the array in order to find the compliment. The space would be O(1).
+
+// ** OPTIMAL SOLUTION :
+
+const twoSum = (nums, target) => {
+	const storedValues = {};
+
+	for (let i = 0; i <= nums.length; i++) {
+		const currNum = nums[i];
+		const neededVal = target - currNum;
+		const secondIndex = storedValue[neededVal]; // If this value is stored in our map then it will tell us what its index.
+
+		// If the value is already stored then it will be the missing index and we can return it.
+		if (secondIndex != null) {
+			return [ i, secondIndex ];
+		} else {
+			storedValues[currNum] = i; // If not, store this element and its index as the value
+		}
+	}
+};
