1073976c12563f5a488b363e53cf6b8c5332ada3
George McCadden III <thethirdone.g@gmail.com>
2020-12-17 18:48:57 -0800

    Stating the problem domain
    This is a common question asked by Facebook and other top software companies

---
+++ twoSum/index.js
@@ -0,0 +1,6 @@
+// ** PROBLEM DOMAIN :
+
+// Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`. These are the constraints ->
+// - You may assume that each input would have exactly ONE solution
+// - You may not use the same element twice
+// - There must be at least two elements
