af4804f4753f6a67e3fe618bea01710b705a01f5
George McCadden III <thethirdone.g@gmail.com>
2020-12-17 23:01:22 -0800

    Solution and complexity analysis

--- twoSum/index.js
+++ twoSum/index.js
@@ -1,43 +1,59 @@
-// ** PROBLEM DOMAIN :
+/*
 
-// Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`. These are the constraints ->
-// - You may assume that each input would have exactly ONE solution
-// - You may not use the same element twice
-// - There must be at least two elements
+PROBLEM DOMAIN :
 
-// ** EXAMPLE 1 :
+Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`. These are the constraints ->
+- You may assume that each input would have exactly ONE solution
+- You may not use the same element twice
+- There must be at least two elements
 
-// Input -> nums = [2, 7, 11, 15], target = 9
-// Output -> [0, 1]
-// Explanation -> Because nums[0] + nums[1] == 9, [0, 1] is returned
+EXAMPLE 1 :
 
-// ** EXAMPLE 2 :
+Input -> nums = [2, 7, 11, 15], target = 9
+Output -> [0, 1]
+Explanation -> Because nums[0] + nums[1] == 9, [0, 1] is returned
 
-// Input -> nums = [3, 2, 4], target = 6
-// Output -> [1, 2]
-// Explanation -> Because nums[1] + nums[2] == 6, [1, 2] is returned
+EXAMPLE 2 :
 
-// ** BRUTE FORCE APPROACH :
+Input -> nums = [3, 2, 4], target = 6
+Output -> [1, 2]
+Explanation -> Because nums[1] + nums[2] == 6, [1, 2] is returned
 
-// - First look at the passed in `target` integer
-// - Loop through the array and, for each element, loop until you find another value that equals to the target
-// Complexity Analysis -> Big O would be O(n^2) time because, for each element, you must loop through the rest of the array in order to find the compliment. The space would be O(1).
+BRUTE FORCE APPROACH :
 
-// ** OPTIMAL SOLUTION :
+- First look at the passed in `target` integer
+- Loop through the array and, for each element, loop until you find another value that equals to the target
+
+COMPLEXITY ANALYSIS : 
+
+Big O would be O(n^2) time because, for each element, you must loop through the rest of the array in order to find the compliment. The space would be O(1).
+
+OPTIMAL SOLUTION :
+
+*/
 
 const twoSum = (nums, target) => {
-	const storedValues = {};
+	const map = {};
 
-	for (let i = 0; i <= nums.length; i++) {
-		const currNum = nums[i];
-		const neededVal = target - currNum;
-		const secondIndex = storedValue[neededVal]; // If this value is stored in our map then it will tell us what its index.
+	for (let i = 0; i < nums.length; i++) {
+		const neededVal = target - nums[i];
 
-		// If the value is already stored then it will be the missing index and we can return it.
-		if (secondIndex != null) {
-			return [ i, secondIndex ];
-		} else {
-			storedValues[currNum] = i; // If not, store this element and its index as the value
+		if (neededVal in map) {
+			return [ map[neededVal], i ];
 		}
+
+		map[nums[i]] = i;
 	}
+
+	return null;
 };
+
+/*
+
+COMPLEXITY ANALYSIS :
+
+Time -> O(n) because I traverse entire array which contains `n` elements and store the values in a hash table. The look up time only costs O(1).
+
+Space -> O(n) because the extra space required depends on the number of items stored in the hash table. At most, the hash table will have `n` elements.
+
+*/
