e1fd40c0c3b36a294a7b152aa2591d4875c64295
George McCadden III <thethirdone.g@gmail.com>
2020-12-18 18:09:30 -0800

    Brute force approach aint the move
    Pay no attention to the algorithm behind the curtain

--- numOfIslands/index.js
+++ numOfIslands/index.js
@@ -9,6 +9,7 @@ Given a 2D grid map of `L`s (land) and `~`s (water), return the number of  islan
 EXAMPLE 1 :
 
 Input -> 
+
 grid = 
 [
     [`L`, `L`, `L`, `L`, `~`],
@@ -16,5 +17,31 @@ grid =
     [`L`, `L`, `~`, `~`, `~`],
     [`~`, `~`, `~`, `~`, `~`]
 ]
+
 Output -> 1
+
+EXAMPLE 2 :
+
+Input -> 
+
+grid =
+[
+    [`L`, `L`, `~`, `~`, `~`],
+    [`L`, `L`, `~`, `~`, `~`],
+    [`~`, `~`, `L`, `~`, `~`],
+    [`~`, `~`, `~`, `L`, `~`]
+]
+
+Output -> 3
+
+
+BRUTE FORCE APPROACH :
+
+- Traverse the entire 2D matrix
+- Keep a count of how much land is present
+- Look up coordinates
+- Return the island count
+
+@param {character[][]} grid
+@return {number}
 */
