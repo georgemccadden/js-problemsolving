3dab771fc8870a919375c40b2a74300199351b3d
George McCadden III <thethirdone.g@gmail.com>
2020-12-18 16:27:34 -0800

    Giving a clear example
    I decided to use ~ for water and L for land because of the visual

--- numOfIslands/index.js
+++ numOfIslands/index.js
@@ -1,7 +1,20 @@
 /*
 PROBLEM DOMAIN :
 
-Given a 2D grid map of `1`s (land) and `0`s (water), return the number of  islands.
+Given a 2D grid map of `L`s (land) and `~`s (water), return the number of  islands.
 
 (note / An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water)
+
+
+EXAMPLE 1 :
+
+Input -> 
+grid = 
+[
+    [`L`, `L`, `L`, `L`, `~`],
+    [`L`, `L`, `~`, `L`, `~`],
+    [`L`, `L`, `~`, `~`, `~`],
+    [`~`, `~`, `~`, `~`, `~`]
+]
+Output -> 1
 */
