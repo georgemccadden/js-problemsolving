45f92481f5f21d20904acf27172293ed48864b2b
George McCadden III <thethirdone.g@gmail.com>
2020-12-18 16:13:49 -0800

    Stating the problem domain

---
+++ numOfIslands/index.js
@@ -0,0 +1,7 @@
+/*
+PROBLEM DOMAIN :
+
+Given a 2D grid map of `1`s (land) and `0`s (water), return the number of  islands.
+
+(note / An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water)
+*/
