74be34598a8ed5b4a95b8c15779a26f5c939c80d
George McCadden III <thethirdone.g@gmail.com>
2020-12-29 09:40:24 -0800

    Giving another clear example

--- subtractProductAndSum/index.js
+++ subtractProductAndSum/index.js
@@ -14,7 +14,13 @@ EXAMPLE 1 :
 
 Input -> n = 234
 Output -> 15
-Explanation -> 
-The product of the digits = 2 * 3 * 4 = 24, the sum of the digits = 2 + 3 + 4 = 9, the difference of 24 - 9 = 15.
+Explanation -> The product of the digits = 2 * 3 * 4 = 24, the sum of the digits = 2 + 3 + 4 = 9, the difference of 24 - 9 = 15.
+
+EXAMPLE 2 :
+
+Input -> n = 4421
+Output -> 21
+Explanation -> The product of the digits = 4 * 4 * 2 * 1 = 32, the sum of the digits = 4 + 4 + 2 + 1 = 11, the difference of 32 - 11 = 21.
+
 
 */
