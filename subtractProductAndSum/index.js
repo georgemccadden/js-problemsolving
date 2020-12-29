0d9874579e2c4970672ac47a03f7228486a9d0f7
George McCadden III <thethirdone.g@gmail.com>
2020-12-29 09:34:05 -0800

    Stating the problem domain

---
+++ subtractProductAndSum/index.js
@@ -0,0 +1,12 @@
+/* 
+
+PROBLEM DOMAIN :
+
+Given an integer number `n`, return the difference between the product of its digits and the sum of its digits.
+
+EDGE CASES ->
+
+- If the integer is just one digit then return that number
+- If there is no passed in integer then print `empty integer` to the console
+
+*/
