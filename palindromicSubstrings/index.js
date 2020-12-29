99681584f99f33a416b740721eb640e875c289bb
George McCadden III <thethirdone.g@gmail.com>
2020-12-28 21:32:08 -0800

    Giving clear examples

--- palindromicSubstrings/index.js
+++ palindromicSubstrings/index.js
@@ -7,6 +7,21 @@ Edge Cases ->
 
 - Palindromes can be counted even if they consist of the same characters.
 - Input strings' length cannot exceed 1000 characters
+- Single characters are considered palindromes
+
+
+EXAMPLE 1 :
+
+Input -> `abc`
+Output -> 3
+Explanation -> There are three palindromic strings: `a`, `b`, `c`
+
+EXAMPLE 2 :
+
+Input -> `aaa`
+Output -> 6
+Explanation -> There are six palindromic strings: `a`, `a`, `a`, `aa`, `aa`, `aaa`
+
 
 
 */
