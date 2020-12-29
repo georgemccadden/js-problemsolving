672a9349f88095d062c4caab03c53ea3e5caeaef
George McCadden III <thethirdone.g@gmail.com>
2020-12-29 00:38:23 -0800

    Optimal solution
    This was successful according to leetcode and put me in the top 5%

--- palindromicSubstrings/index.js
+++ palindromicSubstrings/index.js
@@ -1,4 +1,5 @@
 /* 
+
 PROBLEM DOMAIN :
 
 Given a string, determine how many palindromic substrings are present.
@@ -23,5 +24,32 @@ Output -> 6
 Explanation -> There are six palindromic strings: `a`, `a`, `a`, `aa`, `aa`, `aaa`
 
 
+OPTIMAL SOLUTION :
+
+@param {string} str
+@return {number}
 
 */
+
+const palindromicSubstring = (str) => {
+	/* Determining how long to run based on the time complexity */
+	let length = str.length,
+		howLong = 2 * length - 1,
+		i = 0;
+	let count = 0;
+
+	/* Loop through the string and check for valid palindromes for each character of the string as well as in between them */
+	while (i <= howLong) {
+		let left = Math.floor(i / 2);
+		let right = left + i % 2;
+
+		/* Expanding the substring of characters to see if it's a valid palindrome */
+		while (left >= 0 && right < length && str.charAt(left) == str.charAt(right)) {
+			count++;
+			left--;
+			right++;
+		}
+		i++;
+	}
+	return count;
+};
