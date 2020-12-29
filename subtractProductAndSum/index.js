aa89bd8f3960b1e482a491012f87449035e3ac8e
George McCadden III <thethirdone.g@gmail.com>
2020-12-29 09:37:34 -0800

    Giving a clear example
    It is important that an explanation is given

--- subtractProductAndSum/index.js
+++ subtractProductAndSum/index.js
@@ -9,4 +9,12 @@ EDGE CASES ->
 - If the integer is just one digit then return that number
 - If there is no passed in integer then print `empty integer` to the console
 
+
+EXAMPLE 1 :
+
+Input -> n = 234
+Output -> 15
+Explanation -> 
+The product of the digits = 2 * 3 * 4 = 24, the sum of the digits = 2 + 3 + 4 = 9, the difference of 24 - 9 = 15.
+
 */
