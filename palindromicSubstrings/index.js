e002f0265de9589215d9fc04c6be26e8e94c5a43
George McCadden III <thethirdone.g@gmail.com>
2020-12-28 21:27:43 -0800

    Stating the problem domain
    Also giving the necessary edge cases to think consider

---
+++ palindromicSubstrings/index.js
@@ -0,0 +1,12 @@
+/* 
+PROBLEM DOMAIN :
+
+Given a string, determine how many palindromic substrings are present.
+
+Edge Cases -> 
+
+- Palindromes can be counted even if they consist of the same characters.
+- Input strings' length cannot exceed 1000 characters
+
+
+*/
