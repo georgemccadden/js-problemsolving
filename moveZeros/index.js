5d08522798958572b6b43aeff93cc29307b0672f
George McCadden III <thethirdone.g@gmail.com>
2021-01-06 19:47:14 -0800

    Stating the problem domain and some examples

---
+++ moveZeros/index.js
@@ -0,0 +1,14 @@
+/*
+
+PROBLEM DOMAIN :
+
+Given an array of numbers, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+
+EXAMPLE :
+
+Input -> [0,1,0,3,12]
+Output -> [1,3,12,0,0]
+Explanation -> All of the zeros were moved to the end of the array without changing the order of the non-zero elements (and its the same array, not a copy).
+
+*/
