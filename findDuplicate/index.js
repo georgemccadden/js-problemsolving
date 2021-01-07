cf512a790884ecb7e04ec139d0e379bfdaff4472
George McCadden III <thethirdone.g@gmail.com>
2021-01-07 12:39:40 -0800

    Provided clear examples with needed explanations

--- findDuplicate/index.js
+++ findDuplicate/index.js
@@ -6,4 +6,27 @@ Given an array of integers, nums, containing n + 1 integers where each integer i
 
 (An array is best for accessing elements by index in O(1) time but for searching, inserting, and deleting it is inefficient and takes O(n) time)
 
+
+EXAMPLE 1 :
+
+Input -> nums = [1,3,4,2,2]
+Output -> 2
+
+EXAMPLE 2 :
+
+Input -> nums = [3,1,3,4,2]
+Output -> 3
+
+EXAMPLE 3 :
+
+Input -> nums = [1,1]
+Output -> 1
+
+EXAMPLE 4 :
+
+Input -> nums = [1, 2, 3]
+Output -> undefined
+Explanation -> All the integers are unique and there are none which repeat.
+
+
 */
