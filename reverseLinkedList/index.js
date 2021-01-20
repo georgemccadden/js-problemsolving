16160ef9c851f7f21b86509ff1ee0fec4c15a59f
George McCadden III <thethirdone.g@gmail.com>
2021-01-20 13:58:39 -0800

    Giving a clear example

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -5,4 +5,10 @@ PROBLEM DOMAIN :
 Reverse a singly linked list both iteratively and recursively.
 
 
+EXAMPLE :
+
+Input -> 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+Output -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+
+
 */
