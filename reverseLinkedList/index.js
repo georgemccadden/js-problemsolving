d21806e8dd518d42a1326083c1706bef17781085
George McCadden III <thethirdone.g@gmail.com>
2021-01-20 14:38:37 -0800

    Provided a thorough algorithm
    This is for an iterative solution which I included

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -11,4 +11,37 @@ Input -> 1 -> 2 -> 3 -> 4 -> 5 -> NULL
 Output -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
 
 
+ALGORITHM :
+
+(Iterative)
+- Create variables to keep track of the current node, a previous value, and a next value.
+- Iterate through the linked list until you've reached a node with the value of null.
+- Store the linked list's head node's next value.
+- Update the current node's next value to null.
+- Update the previous node value which was null to the current node's next value.
+- Update the current node's value to the next node in the linked list which was saved.
+- Return the linked list at the end of the iteration.
+
+
+SOLUTIONS :
+
+(Iterative)
+*/
+
+const reverseLinkedListIteratively = (head) => {
+	let curr = head;
+	let next = null;
+	let prev = null;
+
+	while (curr) {
+		next = curr.next;
+		curr.next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return prev;
+};
+
+/*
+(Recursive)
 */
