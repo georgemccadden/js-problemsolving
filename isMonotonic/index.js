065e873833b95b7a1c90b7f303268549a059dae8
George McCadden III <thethirdone.g@gmail.com>
2021-01-24 11:22:21 -0800

    Giving clear examples

--- isMonotonic/index.js
+++ isMonotonic/index.js
@@ -9,5 +9,34 @@ An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is
 Return true if and only if the given array A is monotonic.
 
 
+EXAMPLE 1 :
+
+Input -> [1, 2, 2, 3]
+Output -> true
+
+
+EXAMPLE 2 :
+
+Input -> [6, 5, 4, 4]
+Output -> true
+
+
+EXAMPLE 3 :
+
+Input -> [1, 3, 2]
+Output -> false
+
+
+EXAMPLE 4 :
+
+Input -> [1, 2, 4, 5]
+Output -> true
+
+
+EXAMPLE 5 :
+
+Input -> [1, 1, 1]
+Output -> true
+
 
 */
