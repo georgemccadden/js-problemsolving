878b309d9caef31adac3c8344ebce4301feefe3d
George McCadden III <thethirdone.g@gmail.com>
2021-01-24 11:18:20 -0800

    Stating the problem domain

--- isMonotonic/index.js
+++ isMonotonic/index.js
@@ -4,4 +4,10 @@ PROBLEM DOMAIN :
 
 An array is monotonic if it is either monotone increasing or monotone decreasing.
 
+An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
+
+
+
 */
