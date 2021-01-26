d2988602e39a40558790195c565802336c760bf4
George McCadden III <thethirdone.g@gmail.com>
2021-01-25 21:59:53 -0800

    Writing a clear algorithm

--- isMonotonic/index.js
+++ isMonotonic/index.js
@@ -2,11 +2,7 @@
 
 PROBLEM DOMAIN :
 
-An array is monotonic if it is either monotone increasing or monotone decreasing.
-
-An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
-
-Return true if and only if the given array A is monotonic.
+An array is monotonic if it is either monotone increasing or monotone decreasing. An array A is monotone increasing if for all i <= j, A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j]. Return true if and only if the given array A is monotonic.
 
 
 EXAMPLE 1 :
@@ -37,6 +33,14 @@ EXAMPLE 5 :
 
 Input -> [1, 1, 1]
 Output -> true
+
+
+ALGORITHM :
+
+- Keep a tally of how many elements are incrementing or decrementing.
+- Iterate through the entire array.
+- After you've reached end of the array, if either of the tallies equal the array's length then return `true`.
+- If neither of the tallies equal the length of the array, return `false`.
 
 
 */
