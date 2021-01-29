f0ca1bade726f67daeb8aa8f6d7dc438ebc62558
George McCadden III <thethirdone.g@gmail.com>
2021-01-28 16:53:06 -0800

    No extra white space

--- isMonotonic/index.js
+++ isMonotonic/index.js
@@ -39,8 +39,32 @@ ALGORITHM :
 
 - Keep a tally of how many elements are incrementing or decrementing.
 - Iterate through the entire array.
+- If the value of the current index is less than the next value, add one to the incrementing tally.
+- If the value of the current index is greater than the next value, add one to the decrementing tally.
 - After you've reached end of the array, if either of the tallies equal the array's length then return `true`.
 - If neither of the tallies equal the length of the array, return `false`.
 
+
+SOLUTION :
+*/
+
+const isMonotonic = (array) => {
+	let incrementing = 0;
+	let decrementing = 0;
+
+	if (array[array.length - 1] >= array[array.length - 2]) incrementing++;
+	if (array[array.length - 1] <= array[array.length - 2]) decrementing++;
+
+	while (incrementing !== array.length || decrementing !== array.length) {
+		if (array[i] <= array[i + 1]) incrementing++;
+		if (array[i] >= array[i + 1]) decrementing++;
+	}
+};
+/* 
+BIG O :
+
+Runtime complexity -> O(n) "iterative", because you must travel the entire array at least once in order to determine whether or not the array is monotonic.
+
+Space complexity -> O(1) "constant", you are only keeping track of a few variables which we only add to.
 
 */
