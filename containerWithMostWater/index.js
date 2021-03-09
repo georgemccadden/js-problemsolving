03cf22d59444f567f81170fde5a44de22d0b7645
George McCadden III <thethirdone.g@gmail.com>
2021-03-09 00:04:24 -0800

    Stating the constraints

--- containerWithMostWater/index.js
+++ containerWithMostWater/index.js
@@ -3,4 +3,11 @@
 PROBLEM DOMAIN :
 Given 'n' non-negative integers 'a1, a2, ..., n' , where each represents a point at coordinate (i, ai). 'n' vertical lines are drawn such that the two endpoints of the line 'i' is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water. (You may not slant the container)
 
+
+CONSTRAINTS :
+- Does the thickness of the lines affect the area? -> No, assume they take up no space.
+- Do the left and right sides of the graph count as walls? -> No, the sides cannot be used to form a container.
+- Does a higher line inside of our container affect the area? -> No, lines inside a container does not affect the area.
+
+
 */
