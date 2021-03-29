916907246f05cd69498a56250a91cfe9c0dd21c9
George McCadden III <thethirdone.g@gmail.com>
2021-03-29 16:30:15 -0700

    Listing the constraints

--- trappingRainWater/index.js
+++ trappingRainWater/index.js
@@ -3,4 +3,9 @@
 PROBLEM DOMAIN :
 Given 'n' non-negative representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
+CONSTRAINTS :
+- Do the left and right sides of the graph count as walls? -> No, the sides cannot be used to form a container.
+- Will there be negative integers? -> No, assume that all integers are positive or 0.
+
+
 */
