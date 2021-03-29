73a8389e6ae828104e7dc0a6dc9db7e8e695b7b7
George McCadden III <thethirdone.g@gmail.com>
2021-03-29 16:46:07 -0700

    Test cases for examples

--- trappingRainWater/index.js
+++ trappingRainWater/index.js
@@ -7,5 +7,25 @@ CONSTRAINTS :
 - Do the left and right sides of the graph count as walls? -> No, the sides cannot be used to form a container.
 - Will there be negative integers? -> No, assume that all integers are positive or 0.
 
+TEST CASES :
+[0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+    ^-----^
+       1  ^--------^
+               3   ^-----------^
+                         4
+
+- These values create a graph that holds 8 blocks which represent rain water.
+
+[ ]
+ ^
+- This empty collection would return 0 since there are no values.
+
+[3]
+ ^
+- This collection would return 0 because it doesn't have other values which would properly represent trapped rain water.
+
+[3, 4, 3]
+    ^
+- This collection would return 0 because it doesn't have other values which would properly represent trapped rain water.
 
 */
