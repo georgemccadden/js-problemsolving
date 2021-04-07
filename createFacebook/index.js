a8947d268201b4ca4d690761340d9dc4747a1dea
George McCadden III <thethirdone.g@gmail.com>
2021-04-07 13:13:24 -0700

    Gave a couple test cases

--- createFacebook/index.js
+++ createFacebook/index.js
@@ -3,4 +3,14 @@
 PROBLEM DOMAIN :
 Design a Facebook-like application structure.
 
+TEST CASES :
+username -> happyperson@gmail.com
+password -> iforgotmypassword
+                    ^
+- There should be a user database that checks whether or not the user enters a password incorrectly.
+
+News Feed -> Jesse's Status : I'm looking forward to a great summer!
+                ^
+- After a user has successfully logged in they should be able to see a news feed with other users' statuses.
+
 */
