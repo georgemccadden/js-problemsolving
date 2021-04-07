0932412059976de99cdeacdbb4021a003ebc5228
George McCadden III <thethirdone.g@gmail.com>
2021-04-07 13:26:37 -0700

    Created the database

--- createFacebook/index.js
+++ createFacebook/index.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 
 PROBLEM DOMAIN :
@@ -9,8 +10,32 @@ password -> iforgotmypassword
                     ^
 - There should be a user database that checks whether or not the user enters a password incorrectly.
 
-News Feed -> Jesse's Status : I'm looking forward to a great summer!
+News Feed -> Rafa's Status - I'm looking forward to a great summer of tennis!
                 ^
 - After a user has successfully logged in they should be able to see a news feed with other users' statuses.
 
+BRUTE FORCE SOLUTION :
 */
+const database = [
+	{
+		username: 'happyperson@gmail.com',
+		password: 'password4happyperson!',
+		firstName: 'Rafa',
+		lastName: 'Nadal',
+		status: "I'm looking forward to a great summer of tennis!"
+	},
+	{
+		username: 'sadperson@gmail.com',
+		password: 'password4sadperson!',
+		firstName: 'Michelle',
+		lastName: 'Williams',
+		status: 'If only I could stop limiting myself and be more like Beyoncé..'
+	},
+	{
+		username: 'tallperson@gmail.com',
+		password: 'password4tallperson!',
+		firstName: 'Canden',
+		lastName: 'Poindexter',
+		status: 'I miss picking George up over my head and pretending to throw him down the hallway.. hehe good times!'
+	}
+];
