6beedbfaccf1e1f27df964fd2a355673b13cef69
George McCadden III <thethirdone.g@gmail.com>
2021-04-20 20:14:58 -0700

    Giving some test cases

--- backspaceCompare/index.js
+++ backspaceCompare/index.js
@@ -3,4 +3,20 @@
 PROBLEM DOMAIN :
 Given two strings "s" and "t", return true if they are equal when both are typed into empty text editors. '#' means a backspace character. Note that after backspacing an empty text, the text will continue empty.
 
+
+TEST CASES :
+
+Input -> s = "ab#c", t = "ad#c"
+Output -> true
+Explanation -> Both s and t become "ac".
+
+Input -> s = "ab##", t = "c#d#"
+Output -> true
+Explanation -> Both s and t become "".
+
+
+Input -> s = "a#c", t = "b"
+Output -> false
+Explanation -> s becomes "c" while t becomes "b".
+
 */
