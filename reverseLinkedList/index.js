b213483200e11aa872fa7265b14972756fa4672b
George McCadden III <thethirdone.g@gmail.com>
2021-05-04 22:13:27 -0700

    Test cases

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -3,7 +3,14 @@ PROBLEM DOMAIN :
 Given a linked list, return it in reverse.
 
 TEST CASES :
-Input -> null/single node
-Output -> null/single node
+Input -> 
+null/single node
+Output -> 
+null/single node
+
+Input -> 
+1 -> 2 -> 3 -> 4 -> 5 -> null
+Output ->
+5 -> 4 -> 3 -> 2 -> 1 -> null
 
 */
