6e3230833b730e52d53f7ce6369550407e1b03c2
George McCadden III <thethirdone.g@gmail.com>
2021-05-04 22:36:13 -0700

    Test cases properly written

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -3,14 +3,23 @@ PROBLEM DOMAIN :
 Given a linked list, return it in reverse.
 
 TEST CASES :
-Input -> 
-null/single node
-Output -> 
-null/single node
+null
+ ^
+- Since the input is null it should return null.
 
-Input -> 
+6
+^
+- Since the input is just a single node it should return that node.
+
 1 -> 2 -> 3 -> 4 -> 5 -> null
-Output ->
-5 -> 4 -> 3 -> 2 -> 1 -> null
+			^
 
+- The output should be 5 -> 4 -> 3 -> 2 -> 1 -> null
+
+
+BRUTE FORCE ALGORITHM :
+- Start from the root node
+- Create a current node variable for iteration
+- While the current node is not null, set the current node to the next node.
+- 
 */
