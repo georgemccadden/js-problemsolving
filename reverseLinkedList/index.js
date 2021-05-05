c6e3a64a73650c393ec2ce5ce330168d45602958
George McCadden III <thethirdone.g@gmail.com>
2021-05-05 00:02:07 -0700

    Returning the reversed linked list

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -30,3 +30,14 @@ BRUTE FORCE ALGORITHM :
 
 CODE :
 */
+const reverseLinkedList = (head) => {
+	let prev = null;
+	let curr = head;
+	while (curr) {
+		let next = curr.next;
+		curr.next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return prev;
+};
