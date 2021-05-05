75989be8550bcf0a099c38d10113eeedb0b8cea2
George McCadden III <thethirdone.g@gmail.com>
2021-05-04 23:11:02 -0700

    Brute force algorithm

--- reverseLinkedList/index.js
+++ reverseLinkedList/index.js
@@ -20,6 +20,14 @@ null
 BRUTE FORCE ALGORITHM :
 - Start from the root node
 - Create a current node variable for iteration
-- While the current node is not null, set the current node to the next node.
-- 
+- Create a previous node variable to keep track of the reversal
+- Create a next node variable to indicate where we are in the traversal
+- While the current node is not null,
+	- Update the next node variable to current node's next
+	- Set previous node variable to current node
+	- Set the current node (head) variable's next to null
+	- Update previous node variable as current node's next
+- When current node is null, return the previous node variable for the reversed linked list
+
+CODE :
 */
