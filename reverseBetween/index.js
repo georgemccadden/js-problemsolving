c2a82847b3d3a52d8144f36fda9b92495ab54ed5
George McCadden III <thethirdone.g@gmail.com>
2021-05-18 20:46:46 -0700

    Following the same protocol

--- reverseBetween/index.js
+++ reverseBetween/index.js
@@ -3,11 +3,18 @@ PROBLEM DOMAIN :
 Given the head of a singly linked list and two integers "m" and "n" where "m" <= "n", reverse the nodes of the list from position "m" to position "n", and return the reversed list.
 
 TEST CASES :
+head = {1, 2, 3, 4, 5}, m = 2, n = 4
+                ^
+- Only reversing "m" through "n" returns {1, 4, 3, 2, 5}
 
-Input -> head = {1, 2, 3, 4, 5}, m = 2, n = 4
-Output -> {1, 4, 3, 2, 5}
+head = {5}, m = 1, n = 1
+            ^
+- There is only one node and both "m" and "n" are equal to that node so it returns {5}
 
-Input -> head = {5}, m = 1, n = 1
-Output -> {5}
+head = null, m = 0; n = 0;
+            ^
+- Because both "m" and "n" aren't within the given parameters it returns null
+
+BRUTE FORCE ALGORITHM :
 
 */
